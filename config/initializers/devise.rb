2b35072b2f434d1ef810a40d4ff84371be3bcd92
Grigoriy Dobryakov <grigoriy@dobryakov.com>
2015-09-04 15:41:12 +0300

    remove stub

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,6 +259,7 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
   {
+    :additional_parameters => {"access_type" => "offline"},
     :access_type => "offline",
     :approval_prompt => "",
     :scope => "email, profile, https://www.googleapis.com/auth/calendar",
